960147f05204d7eb7aa8683873e2db9a1bda7d3b
linyi <linyi@tsign.cn>
2019-08-16 15:11:21 +0800

    fix: 字段错误修改

--- app/controllers/shell.js
+++ app/controllers/shell.js
@@ -3,19 +3,22 @@ const { exec } = require("child_process")
 module.exports = {
     doshell(req, res) {
         const shell = req.body.shell
-        const passowrd = req.body.passowrd
-        if (passowrd == "261011") {
+        const password = req.body.password
+        if (password == "261011") {
             exec(shell, (err, stdout, stderr) => {
                 res.json({
                     code: 200,
                     msg: "ok",
-                    err: err
+                    success: true,
+                    err: ""
                 })
             })
         } else {
             res.json({
                 code: 200,
-                msg: "执行失败，密码不正确"
+                msg: "执行失败，密码不正确",
+                success: false,
+                err: true
             })
         }
     }
