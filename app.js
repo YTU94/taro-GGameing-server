d5380ea2e887a94ba19dffa3333baf9ae980ba86
linyi <linyi@tsign.cn>
2019-08-16 15:02:20 +0800

    feat: 设置允许跨域

--- app.js
+++ app.js
@@ -20,6 +20,14 @@ var app = express()
 
 const assert = require("assert")
 app.get("/", (req, res) => res.send("Hello World!"))
+app.all("*", function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    //Access-Control-Allow-Headers ,可根据浏览器的F12查看,把对应的粘贴在这里就行
+    res.header("Access-Control-Allow-Headers", "Content-Type")
+    res.header("Access-Control-Allow-Methods", "*")
+    res.header("Content-Type", "application/json;charset=utf-8")
+    next()
+})
 
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
