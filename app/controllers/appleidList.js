ca297c19d568102e72486c3c81ab4c1982031804
linyi <linyi@tsign.cn>
2019-08-27 18:57:56 +0800

    feat: 获取数据

--- app/controllers/appleidList.js
+++ app/controllers/appleidList.js
@@ -1,14 +1,29 @@
 const pool = require("../../sql/coonPool")
-const common = require("./common")
+const request = require("request")
 
 module.exports = {
     appleidList(req, res) {
-        const sql = "SELECT * from `appleid_list`"
-        pool.coonPool(res, sql, "", response => {
-            res.json({
-                code: 200,
-                msg: "ok",
-                data: response
+        request.get(`https://wxapp.hurbai.com/wxapp1/public/api/portal/articles?page=1&order=-id`, function(error, response, body) {
+            const result = JSON.parse(body)
+            const v = result.data.map(e => {
+                return [e.post_title, e.post_apid, e.post_apidpass]
+            })
+
+            const insertSql = "INSERT INTO `appleid_list` (name, account, password) VALUES ? "
+            const sql = "SELECT * from `appleid_list`"
+            pool.coonPool(res, insertSql, [v], response => {
+                // res.json({
+                //     code: 200,
+                //     msg: "ok",
+                //     data: response
+                // })
+            })
+            pool.coonPool(res, sql, "", response => {
+                res.json({
+                    code: 200,
+                    msg: "ok",
+                    data: response
+                })
             })
         })
     }
