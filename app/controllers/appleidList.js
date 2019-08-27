22556cd368c8770cacbc5c41ef537ff004a696a5
linyi <linyi@tsign.cn>
2019-08-27 15:40:50 +0800

    fix:appleId模块接口

--- app/controllers/appleidList.js
+++ app/controllers/appleidList.js
@@ -4,7 +4,7 @@ const common = require("./common")
 module.exports = {
     appleidList(req, res) {
         const sql = "SELECT * from `appleid_list`"
-        pool.coonPool(res, sql, response => {
+        pool.coonPool(res, sql, "", response => {
             res.json({
                 code: 200,
                 msg: "ok",
