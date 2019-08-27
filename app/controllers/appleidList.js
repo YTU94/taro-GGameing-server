3ce83afb3a5b1916f3e7ab94aefa78dad2a6cb93
linyi <linyi@tsign.cn>
2019-08-27 15:28:35 +0800

    feat:修改 appleId模块

--- app/controllers/appleidList.js
+++ app/controllers/appleidList.js
@@ -4,7 +4,7 @@ const common = require("./common")
 module.exports = {
     appleidList(req, res) {
         const sql = "SELECT * from `appleid_list` order by id desc"
-        pool.coonPool(res, sql, [offsetStart, offseEnd], response => {
+        pool.coonPool(res, sql, response => {
             res.json({
                 code: 200,
                 msg: "ok",
