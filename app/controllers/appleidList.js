51606b14f400494ec802965cf4c4d2a34e3a1aea
linyi <linyi@tsign.cn>
2019-08-27 19:18:07 +0800

    feat: 添加备注

--- app/controllers/appleidList.js
+++ app/controllers/appleidList.js
@@ -6,10 +6,10 @@ module.exports = {
         request.get(`https://wxapp.hurbai.com/wxapp1/public/api/portal/articles?order=-id`, function(error, response, body) {
             const result = JSON.parse(body)
             const v = result.data.map(e => {
-                return [e.post_title, e.post_apid, e.post_apidpass]
+                return [e.post_title, e.post_apid, e.post_apidpass, e.post_excerpt]
             })
             const clearSql = "TRUNCATE TABLE `appleid_list`"
-            const insertSql = "INSERT INTO `appleid_list` (name, account, password) VALUES ? "
+            const insertSql = "INSERT INTO `appleid_list` (name, account, password, remark) VALUES ? "
             const sql = "SELECT * from `appleid_list`"
             pool.coonPool(res, clearSql, "", response => {
                 pool.coonPool(res, insertSql, [v], response => {
