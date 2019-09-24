9ad61452e705664402ff34a2c791fcc9749790c0
linyi <linyi@tsign.cn>
2019-09-24 10:32:27 +0800

    fix: 跨域头 允许Authorization

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use("/", express.static(__dirname + "/"))
 app.all("*", function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*")
     //Access-Control-Allow-Headers ,可根据浏览器的F12查看,把对应的粘贴在这里就行
-    res.header("Access-Control-Allow-Headers", "Content-Type")
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
     res.header("Access-Control-Allow-Methods", "*")
     res.header("Content-Type", "application/json;charset=utf-8")
     next()
