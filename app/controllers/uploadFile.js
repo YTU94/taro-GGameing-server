6423921f90854259e8e0fe259a17eb54f68c0754
linyi <linyi@tsign.cn>
2020-01-07 14:38:52 +0800

    feat : 添加获取文件路径

--- app/controllers/uploadFile.js
+++ app/controllers/uploadFile.js
@@ -1,43 +1,57 @@
-const pool = require("../../sql/coonPool")
-const path = require("path")
-const fs = require("fs")
-const Busboy = require("busboy")
+const pool = require('../../sql/coonPool')
+const path = require('path')
+const fs = require('fs')
+const Busboy = require('busboy')
+const { resolve } = require('path')
 
 module.exports = {
     uploadImg(req, res) {
         //通过请求头信息创建busboy对象
-        let a = ""
+        let a = ''
         let busboy = new Busboy({ headers: req.headers })
 
         //将流链接到busboy对象
         req.pipe(busboy)
 
         //监听file事件获取文件(字段名，文件，文件名，传输编码，mime类型)
-        busboy.on("file", function(filedname, file, filename, encoding, mimetype) {
+        busboy.on('file', function(filedname, file, filename, encoding, mimetype) {
             //创建一个可写流
-            let writeStream = fs.createWriteStream("/www/wwwroot/assets.ytuj.cn/img/" + filename)
-            a = "http://assets.ytuj.cn/img/" + filename
+            let writeStream = fs.createWriteStream('/www/wwwroot/assets.ytuj.cn/img/' + filename)
+            a = 'http://assets.ytuj.cn/img/' + filename
             //监听data事件，接收传过来的文件，如果文件过大，此事件将会执行多次，此方法必须写在file方法里
-            file.on("data", function(data) {
+            file.on('data', function(data) {
                 writeStream.write(data)
             })
 
             //监听end事件，文件数据接收完毕，关闭这个可写流
-            file.on("end", function(data) {
+            file.on('end', function(data) {
                 writeStream.end()
             })
         })
 
         //监听finish完成事件,完成后重定向到百度首页
-        busboy.on("finish", function() {
+        busboy.on('finish', function() {
             res.json({
                 code: 0,
-                msg: "success",
+                msg: 'success',
                 data: {
                     url: a
                 }
             })
             res.end()
+        })
+    },
+    getFilePath(req, res) {
+
+        const path = req.query.path || ''
+        const root '/www/wwwroot/assets.ytuj.cn/'
+        const targetPath = root + path
+        // path = req.query.path
+        let files = fs.readdirSync(targetPath)
+        res.json({
+            code: 0,
+            msg: 'success',
+            data: files
         })
     }
 }
