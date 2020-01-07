7735934cc6a0b47217b7ff121a516cdc4f58023d
linyi <linyi@tsign.cn>
2020-01-07 14:12:18 +0800

    feat: btc接口添加

--- app/controllers/btc.js
+++ app/controllers/btc.js
@@ -16,7 +16,7 @@ module.exports = {
     },
     collectList(req, res) {
         const userId = req.query.userId
-        const sql = "SELECT * FROM `btc` WHERE userId=?"
+        const sql = "SELECT * FROM `btc` WHERE userId=? AND collected=1"
         pool.coonPool(res, sql, userId, response => {
             res.json({
                 code: 0,
@@ -29,13 +29,30 @@ module.exports = {
         const name = req.body.name
         const symbol = req.body.symbol
         const userId = req.body.userId
-        const InsertSql = "INSERT INTO `btc` (name, symbol, userId) VALUES (?,?,?)"
-        pool.coonPool(res, InsertSql, [name, symbol, userId], response => {
-            res.json({
-                code: 0,
-                msg: "ok",
-                data: response
-            })
+        const collected = req.body.collected
+
+        const SelectSql = "SELECT * FROM `btc` WHERE `userId` = ? AND `name` = ?"
+        const UpdateSql = "UPDATE `btc` SET collected = ? WHERE `userId` = ? AND `name` = ?"
+        const InsertSql = "INSERT INTO `btc` (name, symbol, userId, collected) VALUES (?,?,?,?)"
+        pool.coonPool(res, SelectSql, [userId, name], response => {
+            console.log(response)
+            if (response[0]) {
+                pool.coonPool(res, UpdateSql, [collected, userId, name], response => {
+                    res.json({
+                        code: 0,
+                        msg: "ok",
+                        data: response
+                    })
+                })
+            } else {
+                pool.coonPool(res, InsertSql, [name, symbol, userId, collected], response => {
+                    res.json({
+                        code: 0,
+                        msg: "ok",
+                        data: response
+                    })
+                })
+            }
         })
     }
 }
