747cb526e712c9900c6d35b114fc9aa255749201
linyi <linyi@tsign.cn>
2020-06-19 23:13:25 +0800

    chore: 删除不要的文件

---
+++ app/controllers/underCover.js
@@ -0,0 +1,13 @@
+const pool = require("../../sql/coonPool")
+
+module.exports = {
+    // 创建房间 
+    /*
+    * room database
+    /* 
+    // 加入房间
+    // 发牌
+    /*
+    * room database
+    /* 
+}
